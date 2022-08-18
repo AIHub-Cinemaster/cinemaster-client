281f5d95fbf30828539a88710336fbf4a681bdee
jisulee42 <leejisu1584@gmail.com>
2022-08-19 07:18:48 +0900

    img degree

--- src/components/pages/user/MyPick.js
+++ src/components/pages/user/MyPick.js
@@ -1,51 +1,52 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
-import $ from 'jquery'
+import $ from "jquery";
 import MovieModal from "../../modals/MovieModal";
-import emptyBox from "./../../../assets/images/empty.png"
+import emptyBox from "./../../../assets/images/empty.png";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const MyMovieCard=({movieId})=>{
+const MyMovieCard = ({ movieId }) => {
   const [movieInfo, setMovieInfo] = useState([]);
   const [isOpen, setOpen] = useState(false);
 
   const getMovieInfoById = (movie_id) => {
-    axios.get(
-      `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}&language=en-US`
-    ).then(res=>{
-      setMovieInfo(res.data)
-      }).catch(err=>{
-        console.log(err)
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}&language=en-US`
+      )
+      .then((res) => {
+        setMovieInfo(res.data);
       })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getMovieInfoById(movieId);
-  },[])
+  }, []);
 
-  return(
+  return (
     <>
       <div
         className="card set-inline"
-        onClick={()=>{
+        onClick={() => {
           setOpen(true);
-          $('body').css("overflow", "hidden");
-          $('.react-multiple-carousel__arrow').css("display", "none");
+          $("body").css("overflow", "hidden");
+          $(".react-multiple-carousel__arrow").css("display", "none");
         }}
         style={{
-          backgroundImage:
-            `url(https://www.themoviedb.org/t/p/w220_and_h330_face${movieInfo.poster_path})`,
-          margin:"15px 10px"
+          backgroundImage: `url(https://www.themoviedb.org/t/p/w220_and_h330_face${movieInfo.poster_path})`,
+          margin: "15px 10px",
         }}
-      >
-      </div>
+      ></div>
       <MovieModal isOpen={isOpen} setOpen={setOpen} movie_id={movieId} />
     </>
-  )
-}
+  );
+};
 
-const MyPick = ()=>{
+const MyPick = () => {
   const [cookies, setCookie, removeCookie] = useCookies(["userData"]);
   const [myMovieIds, setMyMovieIds] = useState([]);
 
@@ -54,48 +55,48 @@ const MyPick = ()=>{
   }, []);
 
   const getCartList = () => {
-    axios.get(`${process.env.REACT_APP_SERVER_URL}/cart/list/${cookies.userData.shortId}`).then((res) => {
-      if(res.data.empty){
-        setMyMovieIds([])
-        return
-      }
-      //찜영화의 아이디만 담긴 배열
-      setMyMovieIds(res.data.result)
-      // console.log("degv", res.data.empty)
-    }).catch((err) => {
-      console.log(err);
-  });
+    axios
+      .get(
+        `${process.env.REACT_APP_SERVER_URL}/cart/list/${cookies.userData.shortId}`
+      )
+      .then((res) => {
+        if (res.data.empty) {
+          setMyMovieIds([]);
+          return;
+        }
+        //찜영화의 아이디만 담긴 배열
+        setMyMovieIds(res.data.result);
+        // console.log("degv", res.data.empty)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  return(
+  return (
     <>
       <div className="mt-4">
-        <span className="material-icons color-icons">
-          bookmark
-        </span>
+        <span className="material-icons color-icons">bookmark</span>
         <h1 className="white-xl-font set-inline">My Pick</h1>
-
       </div>
-        {
-          myMovieIds.length === 0 ? (
-            <div style={{textAlign:"center"}}>
-              <img src={emptyBox} width="300px" className="m-5"/>
-            </div>
-          ) : (
-            <>
-              {
-                myMovieIds.map((movieId, index)=>(
-                  <MyMovieCard key={index} movieId={movieId} />
-                ))
-              }
-            </>
-          )
-        }
+      {myMovieIds.length === 0 ? (
+        <div style={{ textAlign: "center" }}>
+          <img
+            src={emptyBox}
+            style={{ transform: "rotate(-5deg)" }}
+            width="300px"
+            className="m-5"
+          />
+        </div>
+      ) : (
+        <>
+          {myMovieIds.map((movieId, index) => (
+            <MyMovieCard key={index} movieId={movieId} />
+          ))}
+        </>
+      )}
     </>
-  )
-
-
-
-}
+  );
+};
 
 export default MyPick;
