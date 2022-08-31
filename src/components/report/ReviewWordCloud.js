299d63d05058356eb79c56af4bbb40c8b8d289b1
jisulee42 <leejisu1584@gmail.com>
2022-09-01 06:51:03 +0900

    test

---
+++ src/components/report/ReviewWordCloud.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import ReactWordcloud from 'react-wordcloud';
+
+
+const ReviewWordCloud = ({myContent})=>{
+  const [words, setWords] = useState([]);
+
+  let tempArr = []
+
+  useEffect(()=>{
+    myContent.split(" ").map((sentence)=>{
+      tempArr.push({
+        text: sentence,
+        value: Math.floor(Math.random() * 500)
+      })
+    })
+    setWords(tempArr)
+  },[])
+
+  return (
+    <>
+      <ReactWordcloud words={words} />
+    </>
+  )
+}
+
+export default ReviewWordCloud;
